612475eaa015056361c7b20d098af5a1ed0c9848
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-17 09:16:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 "use strict";
 
+const hero = document.querySelector(".hero");
+const footer = document.querySelector(".footer");
+const navbar = document.querySelector(".navbar");
+
 const mouseFollower = document.querySelector(".mouse-follower");
+
+const navObserver = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navbar.classList.add("hidden");
+        console.log("hide");
+      } else {
+        navbar.classList.remove("hidden");
+        console.log("show");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+const footerObserver = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navbar.classList.add("hidden");
+      } else {
+        navbar.classList.remove("hidden");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+navObserver.observe(hero);
+footerObserver.observe(footer);
 
 window.addEventListener("mousemove", function (event) {
   const { height, width } = mouseFollower.getBoundingClientRect();
