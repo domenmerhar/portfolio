b639d64e146113ccb412fdbe7a501dea54f82040
Domen Merhar <domen.merhar@students.vegova.si>
2024-11-18 07:39:16 +0100

    Update controller.js

---
+++ controller.js
@@ -0,0 +1,101 @@
+"use strict";
+
+import {
+  addRemoveClassOnIntersection,
+  handleCardClick,
+  handleContactBtn,
+  handleMouseMove,
+  removeClassOnIntersection,
+} from "./model.js";
+import {
+  bookViewerCardHTML,
+  contactForm,
+  grandBuyCardHTML,
+  netflixCloneCardHTML,
+} from "./view.js";
+
+const toastDuration = 5000;
+
+const email = "johndoe@gmail.com";
+
+const hero = document.querySelector(".hero");
+const footer = document.querySelector(".footer");
+const navbar = document.querySelector(".navbar");
+
+const mouseFollower = document.querySelector(".mouse-follower");
+const contactBtns = document.querySelectorAll(".contact-email");
+
+const tehcnologies = document.querySelectorAll(".technology");
+const projects = document.querySelectorAll(".projects > div");
+const aboutMeParagraphs = document.querySelectorAll(".about-me p");
+const timeLineTextBoxes = document.querySelectorAll(".time-line__text-box");
+
+const grandBuyCard = document.querySelector(".project__card-big");
+const bookViewerCard = document.querySelector(
+  ".project__card-small:first-of-type"
+);
+const netflixCloneCard = document.querySelector(
+  ".project__card-small:last-of-type"
+);
+
+const lazyLoadable = [
+  ...tehcnologies,
+  ...projects,
+  ...aboutMeParagraphs,
+  ...timeLineTextBoxes,
+];
+
+lazyLoadable.forEach((t) => {
+  t.style.transition = "all 0.2s";
+  t.classList.add("lazy-load");
+});
+
+const navObserver = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      console.log(entry.isIntersecting);
+      if (entry.isIntersecting === false)
+        return navbar.classList.remove("hidden");
+      if (entry.isIntersecting) navbar.classList.add("hidden");
+    });
+  },
+  { threshold: 0.8 }
+);
+
+const footerObserver = new IntersectionObserver(
+  addRemoveClassOnIntersection(navbar, "hidden"),
+  { threshold: 0.1 }
+);
+
+const lazyLoadObserver = new IntersectionObserver(
+  removeClassOnIntersection("lazy-load"),
+  { threshold: 0.5 }
+);
+
+const heroObserver = new IntersectionObserver(
+  function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        footerObserver.observe(footer);
+        navObserver.observe(hero);
+        heroObserver.unobserve(hero);
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+heroObserver.observe(hero);
+
+window.addEventListener("mousemove", handleMouseMove(mouseFollower));
+
+contactBtns.forEach(handleContactBtn(contactForm, toastDuration, email));
+
+grandBuyCard.addEventListener("click", handleCardClick(grandBuyCardHTML));
+bookViewerCard.addEventListener("click", handleCardClick(bookViewerCardHTML));
+netflixCloneCard.addEventListener(
+  "click",
+  handleCardClick(netflixCloneCardHTML)
+);
+
+lazyLoadable.forEach((t) => lazyLoadObserver.observe(t));
